00cdfd1ff455dd1642be29617fe47fba8b610301
EstranhoHUN <xyrohun@gmail.com>
2016-05-24 06:56:12 +0200

    more test cases added

--- src/hu/unideb/inf/estran/ep/test/CoreTest.java
+++ src/hu/unideb/inf/estran/ep/test/CoreTest.java
@@ -2,6 +2,8 @@ package hu.unideb.inf.estran.ep.test;
 
 import static org.junit.Assert.*;
 
+import java.util.Vector;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -10,12 +12,23 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
+import hu.unideb.inf.estran.ep.core.Environment;
 import hu.unideb.inf.estran.ep.core.EvolutionEngine;
+import hu.unideb.inf.estran.ep.core.Population;
+import hu.unideb.inf.estran.ep.core.Unit;
+import hu.unideb.inf.estran.ep.dao.ProjectDao;
+import hu.unideb.inf.estran.ep.dao.ProjectDaoJsonImpl;
+import hu.unideb.inf.estran.ep.dao.ProjectService;
+import hu.unideb.inf.estran.ep.view.Project;
 
 public class CoreTest {
 
-	private EvolutionEngine ee;
-	private EvolutionEngine eeSeeded;
+	private EvolutionEngine ee, eeSeeded;
+	private Environment eOK, eFail, eFail_;
+	private Unit u1, u2;
+	private ProjectDaoJsonImpl pDJ;
+	private ProjectService pS;
+	private Population p;
 
 	@Before
 	public void setUp() throws Exception {
@@ -37,22 +50,57 @@ public class CoreTest {
 		int populationSize = 250;
 		int maxCycle = 250;
 
-		ee = new EvolutionEngine(populationSize, omega.length(), alphabet+ALPHABET+symbols, omega, "", maxCycle, method, weight, mutationRate, differentParents);
+		ee = new EvolutionEngine(populationSize, omega.length(), alphabet+ALPHABET+symbols, "", omega, maxCycle, method, weight, mutationRate, differentParents);
 		eeSeeded = new EvolutionEngine(populationSize, omega.length(), alphabet+ALPHABET+symbols, alpha, omega, maxCycle, method, weight, mutationRate, differentParents);
 
+		eOK = new Environment(populationSize, 12, alphabet+ALPHABET+symbols, alpha, omega);
+		eFail = new Environment(populationSize, 12, alphabet, alpha, omega);
+		eFail_ = new Environment(populationSize, 11, alphabet+ALPHABET+symbols, alpha, omega);
+
+		u1 = new Unit("genome", eOK.calculateFitness("genome"));
+		u2 = new Unit("geneom", eOK.calculateFitness("geneom"));
+
+		pDJ = new ProjectDaoJsonImpl();
+		pS = new ProjectService();
+
+		p = new Population(eOK);
+
+		p.genesis();
 
 	}
 
 
 	@Test
-	public void test() {
-
-		//assertEquals("2*3=?6", 6, test.multiply(2,3));
+	public void instances() {
 
 		assertThat(ee, is(not(eeSeeded)));
-		assertThat(eeSeeded, is(not(ee)));
-		//assertEquals("Should be fail.", ee, eeSeeded);
-		//fail("Not yet implemented");
+		assertThat(u1, is(not(u2)));
+
+		assertThat(ee, instanceOf(EvolutionEngine.class));
+		assertThat(eOK, instanceOf(Environment.class));
+		assertThat(u1, instanceOf(Unit.class));
+		assertThat(pDJ, instanceOf(ProjectDao.class));
+		assertThat(pDJ, instanceOf(ProjectDaoJsonImpl.class));
+		assertThat(pS, instanceOf(ProjectService.class));
 	}
 
+	@Test
+	public void methods(){
+		assertEquals(eOK.isAlphaValid(), true);
+		assertEquals(eOK.isOmegaValid(), true);
+		assertEquals(eFail.isAlphaValid(), false);
+		assertEquals(eFail.isOmegaValid(), false);
+		assertEquals(eFail_.isAlphaValid(), false);
+		assertEquals(eFail_.isOmegaValid(), false);
+		assertEquals(eOK.calculateFitness("Hello World!"), 100);
+		assertEquals(eOK.calculateFitness("Hello World?"), 88);
+		assertEquals(eOK.calculateFitness("XXXXXXXXXXXX"), 0);
+		assertEquals(eOK.calculateFitness("Hello World?"), eOK.calculateFitness("Xello World!"));
+		assertEquals(p.getPopulationFitness()/eOK.getPopulationSize(), p.getavarageFitness());
+		assertEquals(eOK.calculateFitness(u1.getGenome()), eOK.calculateFitness("genome"));
+		assertEquals(u1.getFitness(), eOK.calculateFitness("genome"));
+		assertEquals(p.getPeakFitness(), eOK.calculateFitness(p.getPeakGenome()));
+		assertTrue(p.getPeakFitness() >= 0);
+		assertTrue(p.getAllTimePeakFitness()>=p.getPeakFitness());
+	}
 }
